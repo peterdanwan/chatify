43925dcc82d9e1840c0e14d0342964556225d6b4
peterdanwan <peterdanwan@gmail.com>
2025-12-15 22:37:12 -0500

    feat: serve static React files for backend during production

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@
 
 import express from 'express';
 import stoppable from 'stoppable';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import '#config/dotEnv.js';
 import { logger, shutDownLogger } from '#config/logger.js';
@@ -9,25 +11,35 @@ import authRoutes from '#routes/api/auth.route.js';
 import messageRoutes from '#routes/api/message.route.js';
 
 const serverLog = logger.child({ module: 'server.js' });
-
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// Middleware
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Middleware
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+  // Express v4: app.get('*', (req, res) => ...)
+  // Express v5 ref: https://github.com/expressjs/express/issues/5936#issuecomment-2340677058
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../../frontend', '/dist', 'index.html'));
+  });
+}
+
+// Setting up our server
 const server = stoppable(
   app.listen(PORT, () => {
     serverLog.info(`Server listening on port: ${PORT}`);
   })
 );
 
-// // Graceful shutdown handlers
+// Graceful shutdown handlers
 const gracefulShutdown = (signal) => {
-  // Create a synchronous logger to flush out stdout
-
   shutDownLogger.info(`${signal} received. Starting graceful shutdown...`);
 
   if (server) {
@@ -44,7 +56,7 @@ const gracefulShutdown = (signal) => {
   }
 };
 
-// // Listen for termination signals
+// Listen for termination signals
 process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
 process.on('SIGINT', () => gracefulShutdown('SIGINT'));
 
