4afbc51826c489331b1ff484a1ee954245650b93
peterdanwan <peterdanwan@gmail.com>
2025-12-29 12:47:12 -0500

    !feat: add error handling for db.js
    - NOTE: still figuring out how to run backend via "npm run dev" locally
      and the MongoDB container through a docker run command (for practice's
      sake)
    - Must work on docker-compose.dev.yml file as well

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -8,13 +8,12 @@ const { USE_LOCAL_DB, MONGO_DB_LOCAL_URI, MONGO_DB_URI } = process.env;
 
 export const connectDB = async () => {
   try {
-    const mongoURI =
-      USE_LOCAL_DB === 'true'
-        ? MONGO_DB_LOCAL_URI // Local MongoDB Container (development)
-        : MONGO_DB_URI; // Cloud MongoDB (production)
+    const isLocal = USE_LOCAL_DB === 'true';
+    const mongoURI = isLocal ? MONGO_DB_LOCAL_URI : MONGO_DB_URI;
+    log.info(`Connecting to MongoDB ${isLocal ? 'locally' : 'on the cloud'}`);
 
     if (!mongoURI) {
-      throw new Error('"mongoURI" is not defined. Check your MONGO_DB_LOCAL_URI or MONGO_DB_URI.');
+      throw new Error('"mongoURI" is not defined. Check your MONGO_DB_LOCAL_URI or MONGO_DB_URI');
     }
 
     const conn = await mongoose.connect(mongoURI);
