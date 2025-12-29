e9ad72c09ecf06bb90ebc6c549b84171c4add824
peterdanwan <peterdanwan@gmail.com>
2025-12-29 16:30:09 -0500

    fix: throw error when JWT_SECRET isn't configured

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,17 +4,22 @@ import jwt from 'jsonwebtoken';
 import { parentLogger } from '#config/logger.js';
 
 const log = parentLogger.child({ module: 'utils.js' });
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 // Generate a JWT and send it back as a cookie within the response sent to the client
 export const generateToken = (userId, res) => {
   log.info('Generating a token...');
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+  if (!JWT_SECRET) {
+    throw new Error('JWT_SECRET is not configured');
+  }
+
+  const token = jwt.sign({ userId }, JWT_SECRET, { expiresIn: '7d' });
 
   res.cookie('jwt', token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     httpOnly: true, // prevent XSS attacks (can't access this token via JS): cross-site scripting
     sameSite: 'strict', // prevent CSRF attacks
-    secure: process.env.NODE_ENV === 'development' ? false : true,
+    secure: NODE_ENV === 'development' ? false : true,
   });
 
   log.info('Generated token.');
