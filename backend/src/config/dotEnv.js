5fd20af28159633522eb03b8994b38e131ca7754
peterdanwan <peterdanwan@gmail.com>
2025-12-29 13:01:54 -0500

    docs: add notes regarding dotEnv.js' execution

--- backend/src/config/dotEnv.js
+++ backend/src/config/dotEnv.js
@@ -2,4 +2,10 @@
 
 import dotenv from 'dotenv';
 
+/**
+ * NOTES:
+ * 1. the loading of the .env file within the backend directory (i.e., backend/.env) is a no-op in the container instances.
+ * 2. for container instances, refer to the .env.docker.development & .env.docker.production files. These files are loaded prior to running "npm run dev".
+ */
+
 dotenv.config({ quiet: true });
