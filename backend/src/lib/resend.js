61421ddb9c12f79134ae90af9e5c9a454a0473ee
peterdanwan <peterdanwan@gmail.com>
2026-01-06 17:40:54 -0500

    fix: correct sender.name string

--- backend/src/lib/resend.js
+++ backend/src/lib/resend.js
@@ -5,7 +5,7 @@ import { Resend } from 'resend';
 import { parentLogger } from '#config/logger.js';
 
 const log = parentLogger.child({ module: 'resend.js' });
-const { EMAIL_FROM, EMAIL_FROM_NAME, RESEND_API_KEY } = process.env;
+const { EMAIL_FROM, EMAIL_FROM_FIRST_NAME, EMAIL_FROM_LAST_NAME, RESEND_API_KEY } = process.env;
 
 // Ref: https://resend.com/docs/send-with-nodejs#2-send-email-using-html
 // TODO: compare official documentation with custom code.
@@ -16,7 +16,7 @@ export const resendClient = new Resend(RESEND_API_KEY);
 // The information our users see when they receive emails from us
 export const sender = {
   email: EMAIL_FROM,
-  name: EMAIL_FROM_NAME,
+  name: `${EMAIL_FROM_FIRST_NAME} ${EMAIL_FROM_LAST_NAME}`,
 };
 
 log.info('Configured resend client');
