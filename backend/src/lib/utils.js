2a1522276d5b9d51615fb0183e3f6eed09c1f6f0
peterdanwan <peterdanwan@gmail.com>
2026-01-09 18:16:20 -0500

    fix: rm error-handling of env variables for utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,14 +9,6 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 // Generate a JWT and send it back as a cookie within the response sent to the client
 export const generateToken = (userId, res) => {
   log.debug('Generating a token...');
-  if (!JWT_SECRET) {
-    throw new Error('JWT_SECRET is not configured');
-  }
-
-  if (!NODE_ENV) {
-    throw new Error('NODE_ENV is not configured');
-  }
-
   const token = jwt.sign({ userId }, JWT_SECRET, { expiresIn: '7d' });
 
   res.cookie('jwt', token, {
