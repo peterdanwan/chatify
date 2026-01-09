e739390df56a77c75f3c9b934082b9fe7c06e720
peterdanwan <peterdanwan@gmail.com>
2026-01-09 18:17:10 -0500

    refactor: clear throwing error comments on db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -6,9 +6,6 @@ import { parentLogger } from '#config/logger.js';
 const log = parentLogger.child({ module: 'db.js' });
 const { USE_LOCAL_DB, MONGO_DB_LOCAL_URI, MONGO_DB_URI } = process.env;
 
-// This function can throw 2 errors.
-// 1. When mongoURI isn't defined.
-// 2. When mongoose.connect() cannot connect to the MongoDB database.
 export const connectDB = async () => {
   const isLocal = USE_LOCAL_DB === 'true';
   const mongoURI = isLocal ? MONGO_DB_LOCAL_URI : MONGO_DB_URI;
