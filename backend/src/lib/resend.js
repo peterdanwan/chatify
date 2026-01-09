17beaf38c9d6bfcc17407bb02bf0b881d9a145df
peterdanwan <peterdanwan@gmail.com>
2026-01-09 18:14:51 -0500

    refactor: rm unnecessary log

--- backend/src/lib/resend.js
+++ backend/src/lib/resend.js
@@ -1,13 +1,10 @@
 // backend/src/lib/resend.js
 
 import { Resend } from 'resend';
-import { parentLogger } from '#config/logger.js';
 
-const log = parentLogger.child({ module: 'resend.js' });
 const { EMAIL_FROM, EMAIL_FROM_FIRST_NAME, EMAIL_FROM_LAST_NAME, RESEND_API_KEY } = process.env;
 
 // Ref: https://resend.com/docs/send-with-nodejs#2-send-email-using-html
-
 // We send emails with the resendClient
 export const resendClient = new Resend(RESEND_API_KEY);
 
@@ -16,5 +13,3 @@ export const sender = {
   email: EMAIL_FROM,
   name: `${EMAIL_FROM_FIRST_NAME} ${EMAIL_FROM_LAST_NAME}`,
 };
-
-log.info('Configured resend client');
