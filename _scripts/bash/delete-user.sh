d7187bbc1d56e80052bd6dc39143c64fe10c71f7
peterdanwan <peterdanwan@gmail.com>
2026-01-13 15:19:04 -0500

    fix: standardize fixtures convention to use '-'

--- _scripts/bash/delete-user.sh
+++ _scripts/bash/delete-user.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X DELETE -H "Content-Type: application/json" --data-binary @_fixtures/deleteUser.json localhost:3000/api/auth/delete-user -w '\n'
+curl -X DELETE -H "Content-Type: application/json" --data-binary @_fixtures/delete-user.json localhost:3000/api/auth/delete-user -w '\n'
--- _scripts/node/delete-user.js
+++ _scripts/node/delete-user.js
@@ -8,10 +8,10 @@ import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// This path.join() + __dirname + __filename setup lets us access signup.json relative to signup.js
+// This path.join() + __dirname + __filename setup lets us access delete-user.json relative to delete-user.js
 // Otherwise, when we run our scripts, all filepaths are relative to your "current working directory" (cwd)
-// E.g., if we started at our chatify directory to run "_scripts/node/deleteUser.js", to access deleteUser.json, we'd do "_test/deleteUser.json"
-// E.g., if we started at our backend directory to run "../_scripts/node/deleteUser.js", to access deleteUser.json, we'd do "../_test/deleteUser.json"
+// E.g., if we started at our chatify directory to run "_scripts/node/delete-user.js", to access deleteUser.json, we'd do "_fixtures/deleteUser.json"
+// E.g., if we started at our backend directory to run "../_scripts/node/delete-user.js", to access delete-user.json, we'd do "../_fixtures/deleteUser.json"
 const data = readFileSync(path.join(__dirname, '../../_fixtures/deleteUser.json'), 'utf8');
 
 fetch('http://localhost:3000/api/auth/delete-user', {
