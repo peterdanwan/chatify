ed1372f7e24bc31423592c1639b4fc2e52eed66e
peterdanwan <peterdanwan@gmail.com>
2026-01-13 13:59:06 -0500

    chore: add #middleware/* to imports

--- backend/src/routes/api/auth.route.js
+++ backend/src/routes/api/auth.route.js
@@ -2,14 +2,9 @@
 
 import express from 'express';
 import { parentLogger } from '#config/logger.js';
-import {
-  signup,
-  login,
-  logout,
-  deleteUser,
-  updateProfile,
-  protectRoute,
-} from '#controllers/auth.controller.js';
+import { signup, login, logout, deleteUser, updateProfile } from '#controllers/auth.controller.js';
+
+import { protectRoute } from '#middleware/auth.middleware.js';
 
 const log = parentLogger.child({ module: 'auth.route.js' });
 
