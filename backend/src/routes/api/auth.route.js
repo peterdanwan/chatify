adc766cfaf3f61e685721769fd33d9285121ed74
peterdanwan <peterdanwan@gmail.com>
2026-01-15 12:37:15 -0500

    feat: add arcjet middleware to auth route

--- backend/src/routes/api/auth.route.js
+++ backend/src/routes/api/auth.route.js
@@ -5,10 +5,24 @@ import { parentLogger } from '#config/logger.js';
 import { signup, login, logout, deleteUser, updateProfile } from '#controllers/auth.controller.js';
 
 import { protectRoute } from '#middleware/auth.middleware.js';
+import { arcjetProtection } from '#middleware/arcjet.middleware.js';
 
 const log = parentLogger.child({ module: 'auth.route.js' });
 
 const router = express.Router();
+
+// Instead of doing:
+// router.post('/signup', arcjetProtection, signup);
+// router.post('/login', arcjetProtection, login);
+// router.post('/logout', arcjetProtection, logout);
+// router.put('/update-profile', arcjetProtection, protectRoute, updateProfile);
+// router.delete('/delete-user', arcjetProtection, protectRoute, deleteUser);
+// router.get('/check', arcjetProtection, protectRoute, (req, res) => res.status(200).json(req.user));
+// We can ensure that our arcjetProtection middleware runs before any of our other middleware and route handlers by:
+//  1. placing it in using app.use()
+//  2. calling the next() function within our arcjetProtection function to pass the req, res, and next arguments to the next middleware / route handlers
+// By doing this, we've ensured all our routes are rate limit protected.
+router.use(arcjetProtection);
 
 router.post('/signup', signup);
 router.post('/login', login);
