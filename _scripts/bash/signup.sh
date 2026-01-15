e05edbf8fef659d6cd052c18f7e2acc3e7cdcd92
peterdanwan <peterdanwan@gmail.com>
2026-01-14 19:07:18 -0500

    feat: save the cookie returned by server in signup scripts

--- _scripts/bash/signup.sh
+++ _scripts/bash/signup.sh
@@ -2,4 +2,10 @@
 
 # The -w is necessary. When we normally send a curl command in our terminal, we press "Enter" on our keyboard
 # The -w here sends the command
-curl -X POST -H "Content-Type: application/json" --data-binary @_fixtures/signup.json localhost:3000/api/auth/signup -w '\n'
+curl \
+  -X POST `# Set the HTTP method` \
+  -H "Content-Type: application/json" `# Set the Headers` \
+  --data-binary @_fixtures/signup.json`# Send the Body (JSON payload) from the fixture file` \
+  -c _fixtures/cookies/cookies.txt `# Capture the cookies the server sends back (our JWT token called, 'jwt') into cookies.txt` \
+  localhost:3000/api/auth/signup `# Target URL` \
+  -w '\n' `# Add newline for clean output`
--- _scripts/node/signup.js
+++ _scripts/node/signup.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // _scripts/node/signup.js
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
@@ -13,12 +13,22 @@ const __dirname = path.dirname(__filename);
 // E.g., if we started at our chatify directory to run "_scripts/node/signup.js", to access signup.json, we'd do "_fixtures/signup.json"
 // E.g., if we started at our backend directory to run "../_scripts/node/signup.js", to access signup.json, we'd do "../_fixtures/signup.json"
 const data = readFileSync(path.join(__dirname, '../../_fixtures/signup.json'), 'utf8');
+const cookiePath = path.join(__dirname, '../../_fixtures/cookies/cookies.txt');
 
 fetch('http://localhost:3000/api/auth/signup', {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: data,
 })
-  .then((res) => res.json())
+  .then((res) => {
+    // Capture the Set-Cookie header from the response
+    const setCookie = res.headers.get('set-cookie');
+
+    if (setCookie) {
+      writeFileSync(cookiePath, setCookie);
+    }
+
+    return res.json();
+  })
   .then((json) => console.log(JSON.stringify(json)))
   .catch((err) => console.error('Error', err.message));
