e214eda9beb930806d54372c21eb255ad2117c93
peterdanwan <peterdanwan@gmail.com>
2026-01-15 12:36:20 -0500

    feat: add arcjet middleware

---
+++ backend/src/middleware/arcjet.middleware.js
@@ -0,0 +1,50 @@
+// backend/src/middleware/arcjet.middleware.js
+
+import aj from '#lib/arcjet.js';
+import { isSpoofedBot } from '@arcjet/inspect';
+import { parentLogger } from '#config/logger.js';
+
+const log = parentLogger.child({ module: 'arcjet.middleware.js' });
+
+export const arcjetProtection = async (req, res, next) => {
+  try {
+    // Whether we want to deny or accept the request
+    const decision = await aj.protect(req);
+
+    if (decision.isDenied) {
+      if (decision.reason.isRateLimit()) {
+        log.warn('Arcjet denied request: Rate limit exceeded');
+
+        // Status Code 429: Too Many Requests
+        return res.status(429).json({ message: 'Rate limit exceeded. Please try again later.' });
+      } else if (decision.reason.isBot()) {
+        log.warn('Arcjet denied request: Bot access detected');
+
+        // Status Code 403 = Forbidden
+        return res.status(403).json({ message: 'Bot access denied.' });
+      } else {
+        log.warn('Arcjet denied request: Breach of security policy');
+
+        // Status Code 403 = Forbidden
+        return res.status(403).json({ message: 'Access denied by security policy.' });
+      }
+    }
+
+    // Check for spoofed bots (bots that act like a human and pretend that they are not a bot, making it harder to detect them)
+    // Ref: https://docs.arcjet.com/bot-protection/reference/#check-for-spoofed-bots
+    if (decision.results.some(isSpoofedBot)) {
+      log.warn('Arcjet denied request: Spoofed bot detected');
+
+      // Status Code 403 = Forbidden
+      return res.status(403).json({
+        error: 'Spoofed bot detected.',
+        message: 'Malicious bot activity detected.',
+      });
+    }
+
+    log.info('Arcjet accepted the request');
+  } catch (error) {
+    log.error(error, 'Arcjet Protection Error');
+    next();
+  }
+};
