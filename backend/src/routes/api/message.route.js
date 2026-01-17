0c33c40006d7d492bb03e1b5cd81da04356159bb
peterdanwan <peterdanwan@gmail.com>
2026-01-17 15:04:00 -0500

    docs: what each endpoint is doing

--- backend/src/routes/api/message.route.js
+++ backend/src/routes/api/message.route.js
@@ -22,12 +22,16 @@ const router = express.Router();
 // We have our router run both of these middlewares in this sequence (via router.use()) prior to any route-handling logic
 router.use(arcjetProtection, protectRoute);
 
+// Gets all contacts in our database (PW: why?)
 router.get('/contacts', getAllContacts);
+
+// Gets the chats you have with other individuals
 router.get('/chats', getChatPartners);
 
 // Gets all the messages between the logged in user and another individual who's id we have
 // This is an example of a "dynamic route" (i.e., has a ':' followed by a named value (e.g., 'id'))
 // We can access the value of the id via req.params.id - since id is what we put for this endpoint.
+// From the frontend, we'd click on one of the chatPartners returned from getChatPartners, to get his id
 router.get('/:id', getMessagesByUserId);
 
 // Sends a message to another user by using that other user's id in the request
