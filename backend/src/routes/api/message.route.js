75d7deb38e339249f971f2b986d69ccd256b44eb
peterdanwan <peterdanwan@gmail.com>
2026-01-16 20:12:21 -0500

    refactor: run protection middleware b4 any msg routes

--- backend/src/routes/api/message.route.js
+++ backend/src/routes/api/message.route.js
@@ -15,16 +15,23 @@ const log = parentLogger.child({ module: 'message.route.js' });
 
 const router = express.Router();
 
-router.get('/contacts', arcjetProtection, protectRoute, getAllContacts);
-router.get('/chats', arcjetProtection, protectRoute, getChatPartners);
+// Since all of our message routes should be:
+// 1. Rate limited
+// 2. Protected (via authentication)
+//
+// We can set up our middleware to run prior to any specific route-handling logic via router.use()
+router.use(arcjetProtection, protectRoute);
+
+router.get('/contacts', getAllContacts);
+router.get('/chats', getChatPartners);
 
 // Gets all the messages between the logged in user and another individual who's id we have
 // This is an example of a "dynamic route" (i.e., has a ':' followed by a named value (e.g., 'id'))
 // We can access the value of the id via req.params.id - since id is what we put for this endpoint.
-router.get('/:id', arcjetProtection, protectRoute, getMessagesByUserId);
+router.get('/:id', getMessagesByUserId);
 
 // Sends a message to another user by using that other user's id in the request
-router.post('/send/:id', arcjetProtection, protectRoute, sendMessage);
+router.post('/send/:id', sendMessage);
 
 log.info('Initialized "message" routes');
 
