cc96b42bb26ce144b37edd337a888ce2d4b19cc3
peterdanwan <peterdanwan@gmail.com>
2026-01-26 19:44:13 -0500

    feat: make messageSchema's text field stricter

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -21,6 +21,8 @@ const messageSchema = new mongoose.Schema(
     // Message allows for both text and/or image to be sent
     text: {
       type: String,
+      trim: true,
+      maxLength: 2000,
     },
     image: {
       type: String,
@@ -28,5 +30,16 @@ const messageSchema = new mongoose.Schema(
   },
   { timestamps: true } // createdAt (member since...) + updatedAt (last login...)
 );
+
+// Optimize frequent queries
+// messageSchema.index({ senderId: 1, receiverId: 1, createdAt: -1});
+// messageSchema.index({ receiverId: 1, senderId: 1, createdAt: -1});
+
+// Require at least one of text or image
+// messageSchema.pre("validate", function (next) {
+//  if (!this.text && !this.image) {
+//    return next(new Error("Either text or image is required"));
+//  }
+// });
 
 export const Message = mongoose.model('Message', messageSchema);
