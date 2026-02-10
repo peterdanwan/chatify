fc872e875fd95108a484d3139b72334e5f174dd3
peterdanwan <peterdanwan@gmail.com>
2026-02-10 14:26:25 -0500

    feat: add date-testid attribute to ActiveTab

--- frontend/src/components/ActiveTab.jsx
+++ frontend/src/components/ActiveTab.jsx
@@ -2,11 +2,12 @@
 
 import { useChatStore } from '../store/useChatStore';
 
-function ActiveTab({ activeTabName, children }) {
+function ActiveTab({ activeTabName, children, testId }) {
   const { activeTab, setActiveTab } = useChatStore();
   return (
     <button
-      className={`tab flex-1 ${activeTab === activeTabName ? 'tab-active bg-cyan-500/20 text-cyan-400' : 'text-slate-400'}`}
+      date-testid={testId} // No attribute rendered if testId is undefined, null, or false. Having an empty string would cause tate-testid to show up.
+      className={`active-tab tab flex-1 ${activeTab === activeTabName ? 'tab-active bg-cyan-500/20 text-cyan-400' : 'text-slate-400'}`}
       role="tab"
       onClick={() => {
         setActiveTab(activeTabName);
