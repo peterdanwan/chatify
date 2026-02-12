85d1dbda478d4260227dd6c6b5e4ea58da32c69c
peterdanwan <peterdanwan@gmail.com>
2026-02-12 16:27:36 -0500

    feat: make isOnline logic stricter & add tooltip

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -9,7 +9,8 @@ import { useAuthStore } from '../store/useAuthStore';
 export default function ChatHeader() {
   const { selectedUser, setSelectedUser } = useChatStore();
   const { onlineUsers } = useAuthStore();
-  const isOnline = onlineUsers.includes(selectedUser._id);
+  const isOnline = selectedUser?._id ? onlineUsers.includes(String(selectedUser._id)) : false;
+  // const isOnline = onlineUsers.includes(selectedUser._id);
 
   useEffect(() => {
     const handleEscKey = (event) => {
@@ -25,7 +26,7 @@ export default function ChatHeader() {
   return (
     <div
       id="chat-header"
-      className="flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-21 px-6 flex-1"
+      className="flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-21 px-1 flex-1"
     >
       <div className="flex items-center space-x-3 ">
         <div className={`avatar ${isOnline ? 'avatar-online' : 'avatar-offline'}`}>
@@ -43,7 +44,10 @@ export default function ChatHeader() {
           </div>
         </div>
         <div>
-          <h3 className="text-slate-200 font-medium">{`${selectedUser.firstName} ${selectedUser.lastName}`}</h3>
+          <h3
+            className="text-slate-200 font-medium tooltip tooltip-top"
+            data-tip={`${selectedUser.firstName} ${selectedUser.lastName}`}
+          >{`${selectedUser.firstName} ${selectedUser.lastName}`}</h3>
           <p className="text-slate-400 text-sm">{isOnline ? 'Online' : 'Offline'}</p>
         </div>
 
