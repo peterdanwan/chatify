8ee35c44f214112ea06308bfa81da7699292743a
peterdanwan <peterdanwan@gmail.com>
2026-02-12 16:07:57 -0500

    styles: remove height styles on login/signup

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -22,14 +22,17 @@ function LoginPage() {
   };
 
   return (
-    <div id="login-page" className="w-full flex items-center justify-center p-4 bg-slate-900 ">
+    <div id="login-page" className="w-full flex items-center justify-center bg-slate-900 ">
       {/* Max width is set. height is 650px on small screens, and height is 800px when on medium sized screens and above */}
       {/* h-[560px] replaced with h-162.5 | md:h-[800px] replaced with */}
-      <div className="w-full relative max-w-6xl h-162.5 md:h-200">
+      <div className="w-full relative max-w-6xl">
         <BorderAnimatedContainer>
           <div className="w-full flex flex-col md:flex-row">
             {/* FORM + INPUT FIELDS - LEFT SIDE */}
-            <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30">
+            <div
+              id="left-side"
+              className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30"
+            >
               <div className="w-full max-w-md">
                 {/* HEADING TEXT */}
                 <div className="justify-items-center mb-6 ">
@@ -82,7 +85,10 @@ function LoginPage() {
             </div>
 
             {/* LOGIN IMAGE - RIGHT SIDE*/}
-            <div className="hidden md:w-1/2 md:flex items-center justify-center p-6 bg-linear-to-bl from-slate-800/20 to-transparent">
+            <div
+              id="right-side"
+              className="hidden md:w-1/2 md:flex items-center justify-center p-8 bg-linear-to-bl from-slate-800/20 to-transparent"
+            >
               <div>
                 <img
                   src="/login.png"
