9455b9cd6237997a6fc096ce7c03db33fee67009
peterdanwan <peterdanwan@gmail.com>
2026-02-13 13:46:59 -0500

    styles: rm fixed height on img

--- frontend/src/components/ChatMessage.jsx
+++ frontend/src/components/ChatMessage.jsx
@@ -12,7 +12,7 @@ function ChatMessage({ msg }) {
       <div
         className={`chat-bubble relative ${msg.senderId === authUser._id ? 'bg-cyan-600 text-white' : 'bg-slate-800 text-slate-200'}`}
       >
-        {msg.image && <img src={msg.image} alt="Shared" className="rounded-lg h-8 object-cover" />}
+        {msg.image && <img src={msg.image} alt="Shared" className="rounded-lg object-cover" />}
         {msg.text && <p className="mt-1">{msg.text}</p>}
         <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
           {new Date(msg.createdAt).toLocaleTimeString([], {
