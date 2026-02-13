90c709d8f6801152634e2c9771858fe7f04ffcc3
peterdanwan <peterdanwan@gmail.com>
2026-02-13 12:48:02 -0500

    feat: add subscribeToMessages + unsubscribeFromMessages

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -125,4 +125,37 @@ export const useChatStore = create((set, get) => ({
       //
     }
   },
+
+  subscribeToMessages: () => {
+    const { selectedUser, isSoundEnabled } = get();
+
+    if (!selectedUser) {
+      return;
+    }
+
+    const socket = useAuthStore.getState().socket;
+
+    socket.on('newMessage', (newMessage) => {
+      // !!
+      const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+      if (!isMessageSentFromSelectedUser) {
+        return;
+      }
+
+      const currentMessages = get().messages;
+      set({ messages: [...currentMessages, newMessage] });
+
+      if (isSoundEnabled) {
+        const notificationSound = new Audio('/sounds/notification.mp3');
+
+        notificationSound.currentTime = 0; // Reset to start
+        notificationSound.play().catch((error) => console.log('Audio play failed:', error));
+      }
+    });
+  },
+
+  unsubscribeFromMessages: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.off('newMessage');
+  },
 }));
