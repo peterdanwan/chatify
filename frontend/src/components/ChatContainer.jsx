d9c9f75724d9797e73da5b2fd5fcccac15a31df3
peterdanwan <peterdanwan@gmail.com>
2026-02-15 13:38:38 -0500

    fix: rm states and useEffect from ChatContainer

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,22 +1,10 @@
 // frontend/src/components/ChatContainer.jsx
 
-import { useEffect } from 'react';
-import { useChatStore } from '../store/useChatStore';
 import ChatHeader from './ChatHeader';
 import ChatBody from './ChatBody';
 import MessageInput from './MessageInput';
 
 function ChatContainer() {
-  const { selectedUser, getMessagesByUserId, subscribeToMessages, unsubscribeFromMessages } =
-    useChatStore();
-
-  useEffect(() => {
-    getMessagesByUserId(selectedUser._id);
-    subscribeToMessages();
-
-    return () => unsubscribeFromMessages();
-  }, [selectedUser, getMessagesByUserId, subscribeToMessages, unsubscribeFromMessages]);
-
   return (
     <div id="chat-container" className="flex flex-col flex-1 overflow-y-auto">
       <ChatHeader />
