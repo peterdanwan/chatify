cc5135a4785de04c83fd1f1e44b821e56c9b3faa
peterdanwan <peterdanwan@gmail.com>
2026-03-08 11:07:51 -0400

    styles: remove h-full on BorderAnimatedContainer

--- frontend/src/components/BorderAnimatedContainer.jsx
+++ frontend/src/components/BorderAnimatedContainer.jsx
@@ -7,7 +7,7 @@ function BorderAnimatedContainer({ children }) {
   return (
     <div
       id="border-animated-container"
-      className="w-full h-full border overflow-hidden flex flex-col lg:flex-row border-slate-700
+      className="w-full  border overflow-hidden flex flex-col lg:flex-row border-slate-700
       bg-slate-800/30 rounded-none lg:rounded-2xl
       lg:[background:linear-gradient(45deg,#172033,--theme(--color-slate-800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),--theme(--color-slate-600/.48)_80%,--theme(--color-cyan-500)_86%,--theme(--color-cyan-300)_90%,--theme(--color-cyan-500)_94%,--theme(--color-slate-600/.48))_border-box] 
       lg:border-transparent lg:animate-border"
